d7c8695574cc8b13dcbf32f3372ff1ad985aebf3
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-16 21:59:15 +0600

    json file added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div>
+      {/* <Header></Header> */}
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
       </Routes>
+      {/* <Footer></Footer> */}
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is Header</h1>
+            <h1>This is header</h1>
         </div>
     );
 };
