ad53c6e073faa4462aa890dc63d3ac03869790a8
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-17 00:49:13 +0600

    Service shadow + Footer Done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react';
-
+import './Footer.css'
 const Footer = () => {
-    let year =  new Date().getFullYear();
+    let year = new Date().getFullYear();
     console.log(year);
     return (
-        <footer className='text-center mt-5'>    
-            <p>Copyright © {year} Pics Diary</p>
-        </footer>
+        <div className='footer'>
+            <div className='footer-container'>
+                <div>
+                    <h2>Who We Are</h2>
+                    <p>You’ve worked very hard planning for your “Big Day”. You almost want everything to go flawless. There will be so much happening that day. The last thing you should worry about if everything is being documented properly. That’s why we recommend you full day coverage package – one less thing for you to worry about. We will be with you from the very beginning of your day till the end. That means, you just enjoy to the fullest and look fabulous.</p>
+                </div>
+                <div>
+                    <h2>Contact With Us</h2>
+                    <p>Address: House 08, Road 08, Block D, Mirpur 2, Dhaka 1216</p>
+                    <p>Phone: +880171200000</p>
+                    <p>Email: support@pics-diary.com</p>
+                    <p>Office Schedule: SAT - THUS (10 AM - 7 PM)</p>
+                </div>
+            </div>
+            <p className='text-center pb-2'>Copyright © {year} Pics Diary</p>
+        </div>
     );
 };
 
