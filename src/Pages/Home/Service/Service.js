e249ba6acf11a1d6cc8fb4a9b6f1526347a2523a
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-16 23:04:23 +0600

    responsive navbar modified

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -7,7 +7,7 @@ const Service = ({service}) => {
         <div className='service'>
             <img className='w-100' src={img} alt="" />
             <h4>{name}</h4>
-            <p><strong>Price: {price} BDT</strong></p>
+            <p className='service-price'><strong>Price: {price} BDT</strong></p>
             <p className='service-description'><small>{description}</small></p>
             <button>Book {name}</button>
         </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Pics Diary</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    </Nav>
+                    <Nav className='navbar-link'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/about'>About</Link>
+                        <Link to='/about'>About</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
