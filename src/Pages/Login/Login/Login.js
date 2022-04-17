b28997cc8ef07a68d3ad037b5c503ce5a5387eeb
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-17 21:13:02 +0600

    login system started

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -4,8 +4,8 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import Loading from '../../Shared/Loading/Loading';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+
 
 const Login = () => {
     const emailRef = useRef('');
@@ -75,7 +75,6 @@ const Login = () => {
             {errorElement}
             <p>New to Genius Car? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link> </p>
             <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
-            <SocialLogin></SocialLogin>
             <ToastContainer />
         </div>
     );
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import google from '../../../images/social/google.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
+
+const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    
+    let errorElement;
+
+    if(loading){
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
+    return (
+        <div>
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className='bg-primary w-50'></div>
+            </div>
+            {errorElement}
+            <div className=''>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-info w-50 d-block mx-auto my-2'>
+                    <img style={{ width: '30px' }} src={google} alt="" />
+                    <span className='px-2'>Google Sign In</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
