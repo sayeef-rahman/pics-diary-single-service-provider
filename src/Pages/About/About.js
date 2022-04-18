5cd060bf23f4e370003fb7d947aa581a72024a58
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-18 13:25:48 +0600

    about page completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import sayeef from '../../images/sayeef.jpg'
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About</h1>
+        <div className='text-center'>
+            <h4>About Myself</h4>
+            <img className='profile-image' src={sayeef} alt="" />
+            <h6 className='mt-3'><strong>Sayeef Rahman</strong></h6>
+            <p><small>Social Media Marketing Specialist @ Digitarly</small></p>
         </div>
     );
 };
