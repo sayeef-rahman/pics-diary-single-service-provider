6ccef13499d51d908bae3ee6c2408127bf1bc3ca
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-18 13:16:17 +0600

    checkout page completed

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
-    const { serviceId } = useParams();
+    const { serviceID } = useParams();
+    console.log("In Service Detail: ",serviceID);
     return (
         <div className='text-center mt-3'>
-            <h1>You have selected service number: {serviceId}</h1>
+            <h1>You have selected service number: {serviceID}</h1>
             <div className='m-3'>
                 <Link to="/checkout">
                     <button className='btn btn-primary'>Proceed Checkout</button>
