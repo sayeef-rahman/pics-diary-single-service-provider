cfb5367b6da682556da442bc58f6040c15816437
sayeef-rahman <sayeef.cse@gmail.com>
2022-04-18 12:35:41 +0600

    checkout page modified

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Checkout = () => {
     return (
         <div>
-            <h2>Please Checkout your booking</h2>
+            <h2 className='text-center text-success'>Please Checkout your booking. Pay with online & mobile banking</h2>
         </div>
     );
 };
